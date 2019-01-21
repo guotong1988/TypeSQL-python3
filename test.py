9f993356dedd0a888a748dc81885fdf80ce1f170
Tong Guo <779222056@qq.com>
2019-01-21 15:02:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -9,9 +9,9 @@ from typesql.model.sqlnet import SQLNet
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--toy', action='store_true',
+    parser.add_argument('--toy', action='store_true', default=False,
             help='If set, use small data; used for fast debugging.')
-    parser.add_argument('--sd', type=str, default='',
+    parser.add_argument('--sd', type=str, default='saved_model_kg',
             help='set model save directory.')
     parser.add_argument('--db_content', type=int, default=0,
             help='0: use knowledge graph type, 1: use db content to get type info')
@@ -46,26 +46,26 @@ if __name__ == '__main__':
     model = SQLNet(word_emb, N_word=N_word, gpu=GPU, trainable_emb = args.train_emb, db_content=args.db_content)
 
     agg_m, sel_m, cond_m, agg_e, sel_e, cond_e = best_model_name(args)
-    print "Loading from %s"%agg_m
+    print("Loading from %s"%agg_m)
     model.agg_pred.load_state_dict(torch.load(agg_m))
-    print "Loading from %s"%sel_m
+    print("Loading from %s"%sel_m)
     model.selcond_pred.load_state_dict(torch.load(sel_m))
-    print "Loading from %s"%cond_m
+    print("Loading from %s"%cond_m)
     model.op_str_pred.load_state_dict(torch.load(cond_m))
     #only for loading trainable embedding
-    print "Loading from %s"%agg_e
+    print("Loading from %s"%agg_e)
     model.agg_type_embed_layer.load_state_dict(torch.load(agg_e))
-    print "Loading from %s"%sel_e
+    print("Loading from %s"%sel_e)
     model.sel_type_embed_layer.load_state_dict(torch.load(sel_e))
-    print "Loading from %s"%cond_e
+    print("Loading from %s"%cond_e)
     model.cond_type_embed_layer.load_state_dict(torch.load(cond_e))
 
 
-    print "Dev acc_qm: %s;\n  breakdown on (agg, sel, where): %s"%epoch_acc(
-            model, BATCH_SIZE, val_sql_data, val_table_data, TEST_ENTRY, args.db_content)
-    print "Dev execution acc: %s"%epoch_exec_acc(
-            model, BATCH_SIZE, val_sql_data, val_table_data, DEV_DB, args.db_content)
-    print "Test acc_qm: %s;\n  breakdown on (agg, sel, where): %s"%epoch_acc(
-            model, BATCH_SIZE, test_sql_data, test_table_data, TEST_ENTRY, args.db_content)
-    print "Test execution acc: %s"%epoch_exec_acc(
-            model, BATCH_SIZE, test_sql_data, test_table_data, TEST_DB, args.db_content)
+    print("Dev acc_qm: %s;\n  breakdown on (agg, sel, where): %s"%epoch_acc(
+            model, BATCH_SIZE, val_sql_data, val_table_data, TEST_ENTRY, args.db_content))
+    print("Dev execution acc: %s"%epoch_exec_acc(
+            model, BATCH_SIZE, val_sql_data, val_table_data, DEV_DB, args.db_content))
+    print("Test acc_qm: %s;\n  breakdown on (agg, sel, where): %s"%epoch_acc(
+            model, BATCH_SIZE, test_sql_data, test_table_data, TEST_ENTRY, args.db_content))
+    print("Test execution acc: %s"%epoch_exec_acc(
+            model, BATCH_SIZE, test_sql_data, test_table_data, TEST_DB, args.db_content))
